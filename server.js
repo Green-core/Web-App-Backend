9adfcd4d5670219184462394a90d03f699a92691
Binara Medawatta <binaramedawatta@gmail.com>
2020-10-11 23:07:30 +0530

    removed origin

--- server.js
+++ server.js
@@ -26,11 +26,11 @@ mongoose
     process.exit();
   });
 
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://rocky-mesa-26212.herokuapp.com/"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+  // app.use(function(req, res, next) {
+  //   res.header("Access-Control-Allow-Origin", "https://rocky-mesa-26212.herokuapp.com/"); // update to match the domain you will make the request from
+  //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  //   next();
+  // });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
