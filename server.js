10aa47e0d5cc803fbc3abfdda3a356a96b7ca6a4
Binara Medawatta <binaramedawatta@gmail.com>
2020-10-11 22:59:26 +0530

    changed origin

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ mongoose
   });
 
   app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://gcwafe.herokuapp.com/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "https://rocky-mesa-26212.herokuapp.com/"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
